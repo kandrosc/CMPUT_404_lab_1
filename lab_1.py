c8b3362c2092a0e884afd744bb8f19012a7ecfa8
kandrosc <kandrosc@ualberta.ca>
2021-01-14 17:24:52 -0500

    modified script to return raw script text from github

--- lab_1.py
+++ lab_1.py
@@ -2,6 +2,6 @@ import requests
 
 
 if __name__ == "__main__":
-	r = requests.get("http://google.com")
+	r = requests.get("https://raw.githubusercontent.com/kandrosc/CMPUT_404_lab_1/master/lab_1.py")
 	print("Returned a status of "+str(r.status_code))
 	print(r.text)
